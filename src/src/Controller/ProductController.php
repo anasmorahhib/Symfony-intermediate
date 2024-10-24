8478986798f4e7a9f6a8e6a2dfc229a557adca4d
Anas Morahhib <anas.morahhib@sofrecom.com>
2024-10-24 13:07:50 +0100

    Update Ux

--- src/src/Controller/ProductController.php
+++ src/src/Controller/ProductController.php
@@ -8,46 +8,7 @@ use Twig\Environment;
 
 class ProductController extends AbstractController
 {
-    public $products = [];
-
-    public function __construct()
-    {
-        $this->products = [
-            [
-                'id' => 0,
-                'title' => 'Casque Audio',
-                'price' => '500 ',
-                'image' => 'headphone.jpg',
-                'description' => 'Casque audio de haute qualité avec réduction de bruit et confort optimal.',
-                'created_at' => new \DateTime('2023-09-11')
-            ],
-            [
-                'id' => 1,
-                'title' => 'Portable',
-                'price' => '7200 ',
-                'image' => 'phone.jpg',
-                'description' => 'Smartphone dernier cri avec un écran AMOLED et une excellente performance.',
-                'created_at' => new \DateTime('2023-10-12')
-            ],
-            [
-                'id' => 2,
-                'title' => 'Montre Intelligente',
-                'price' => '600 ',
-                'image' => 'watch.jpg',
-                'description' => 'Montre intelligente avec suivi de l’activité et notifications en temps réel.',
-                'created_at' => new \DateTime('2023-10-11')
-            ],
-            [
-                'id' => 3,
-                'title' => 'Caméra',
-                'price' => '4550 ',
-                'image' => 'camera.jpg',
-                'description' => 'Caméra numérique avec une résolution élevée et plusieurs modes de prise de vue.',
-                'created_at' => new \DateTime('2023-10-10')
-            ],
-        ];
-
-    }
+    
     // Routes config file
     public function products(DateTimeFormatter $dateTimeFormatter)
     {
