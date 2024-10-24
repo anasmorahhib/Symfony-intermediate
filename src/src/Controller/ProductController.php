f904a593c820281acccace031c595d384e576413
Anas Morahhib <anas.morahhib@sofrecom.com>
2024-10-24 15:01:17 +0100

    Http Client Service

--- src/src/Controller/ProductController.php
+++ src/src/Controller/ProductController.php
@@ -4,25 +4,31 @@ use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Twig\Environment;
 
 class ProductController extends AbstractController
 {
-    
+
     // Routes config file
-    public function products(DateTimeFormatter $dateTimeFormatter)
+    public function products(HttpClientInterface $httpClient)
     {
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/anasmorahhib/formation_symfony6/refs/heads/main/0.data/products.json');
+        $products = $response->toArray();
+
         return $this->render(
             'product/list.html.twig',
-            ['products' => $this->products]
+            ['products' => $products]
         ); // render return a Response
     }
 
-    public function product($id, Environment $twig)
+    public function product($id, Environment $twig, HttpClientInterface $httpClient)
     {
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/anasmorahhib/formation_symfony6/refs/heads/main/0.data/products.json');
+        $products = $response->toArray();
         $html = $twig->render(
             'product/single.html.twig',
-            ['product' => $this->products[$id]]
+            ['product' => $products[$id]]
         );
 
         return new Response($html);
