6696c663f9cff120e299bc0f2d92ee0d0e2a18b0
Anas Morahhib <anas.morahhib@sofrecom.com>
2024-10-28 17:14:14 +0100

    Named autowiring & Non-Autowireable Service

--- src/src/Service/ProductRepository.php
+++ src/src/Service/ProductRepository.php
@@ -2,32 +2,41 @@
 
 namespace App\Service;
 
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+
 
 class ProductRepository
 {
     private HttpClientInterface $httpClient;
     private CacheInterface $cache;
     private bool $isDebug;
+    private DebugCommand $twigDebugCommand;
 
-    public function __construct(HttpClientInterface $httpClient, CacheInterface $cache, bool $isDebug)
+    public function __construct(DebugCommand $twigDebugCommand, HttpClientInterface $githubContentClient, CacheInterface $cache, bool $isDebug)
     {
-        $this->httpClient = $httpClient;
+        $this->httpClient = $githubContentClient;
         $this->cache = $cache;
         $this->isDebug = $isDebug;
-
+        $this->twigDebugCommand = $twigDebugCommand;
     }
 
     public function findAll()
     {
+        $output = new BufferedOutput();
+        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+        dump($output);
+
         return $this->cache->get(
             'products_data',
             function ($productCache) {
                 $productCache->expiresAfter($this->isDebug ? 60 : 1080);
                 $response = $this->httpClient->request(
                     'GET',
-                    'https://raw.githubusercontent.com/anasmorahhib/formation_symfony6/refs/heads/main/0.data/products.json'
+                    '/anasmorahhib/formation_symfony6/refs/heads/main/0.data/products.json'
                 );
                 return $response->toArray();
             }
