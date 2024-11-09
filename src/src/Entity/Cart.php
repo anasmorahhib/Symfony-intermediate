a2b28d945b54ed1a77ef5dfd5ce551b31a600265
Anas Morahhib <anas.morahhib@sofrecom.com>
2024-11-09 16:33:30 +0100

    Timestampable

--- src/src/Entity/Cart.php
+++ src/src/Entity/Cart.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use App\Repository\CartRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: CartRepository::class)]
 class Cart
 {
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
